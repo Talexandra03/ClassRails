9cc3b68fb6200c4903b174af9121ac91c5e7f2b1
TatianaAcevedo <Tatianacgrafip@gmail.com>
2018-10-03 16:29:10 -0500

    Metodos crear y mostrardel controlador tareas

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -1,14 +1,28 @@
 class TareasController < ApplicationController
+  
   def index
   	@tareas = Tarea.all
   	#select * from tareas
   end
+
   def new
   	@tarea = Tarea.new
 
-  	def create
-  		
-  	end
-  	
   end
+
+  	def create
+  		@tarea= Tarea.new(titulo: params[:tarea][:titulo], descripcion: params[:tarea][:descripcion])
+        
+        if @tarea.save 
+        #insert into tareas (titulo,descripcion) values (formulario)
+        else
+        render :new
+        end
+  	end
+   
+   def show
+      @tarea = Tarea.find(params[:id])
+            #select * from tareas where id=:id
+    end
+            	
 end
