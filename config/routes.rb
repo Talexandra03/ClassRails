f1ac5a9c3077f0206b6e9f4b110078cc68a3b497
TatianaAcevedo <Tatianacgrafip@gmail.com>
2018-10-04 16:33:32 -0500

    Metodo Editar y eliminar

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'tareas/:id', to: 'tareas#show'
   delete 'tareas/:id', to: 'tareas#destroy'
   get 'tareas/:id/edit', to: 'tareas#edit'
+  put 'tareas/:id' to: 'tareas#update'
 
 
   get 'bienvenido/index'
